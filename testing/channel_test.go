bf1fa11c0e3b1aa92138a77d313214590d7f1977
tangguhriyadi <mtangguh97@gmail.com>
2023-05-20 08:50:48 +0700

    channel as parameter

--- testing/channel_test.go
+++ testing/channel_test.go
@@ -20,3 +20,19 @@ func TestCreateChannel(t *testing.T) {
 	fmt.Println(data)
 	time.Sleep(5 * time.Second)
 }
+
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Tangguh"
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go GiveMeResponse(channel)
+
+	data := <-channel
+	fmt.Println(data)
+	time.Sleep(5 * time.Second)
+}

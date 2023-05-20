dca9830988b1c2fe6b947ff3653fb0c1cfa8fab6
tangguhriyadi <mtangguh97@gmail.com>
2023-05-20 09:42:34 +0700

    channel in out

--- testing/channel_test.go
+++ testing/channel_test.go
@@ -36,3 +36,24 @@ func TestChannelAsParameter(t *testing.T) {
 	fmt.Println(data)
 	time.Sleep(5 * time.Second)
 }
+
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Tangguh"
+}
+
+func OnlyOut(channel <-chan string) {
+	time.Sleep(2 * time.Second)
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(5 * time.Second)
+}

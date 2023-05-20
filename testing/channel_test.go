da88f86e4ab4ae97a70050e4186de6445bf46d6d
tangguhriyadi <mtangguh97@gmail.com>
2023-05-20 16:06:57 +0700

    sync map

--- testing/channel_test.go
+++ testing/channel_test.go
@@ -257,3 +257,26 @@ func TestPool(t *testing.T) {
 	time.Sleep(3 * time.Second)
 	fmt.Println("Done")
 }
+
+func AddToMap(data *sync.Map, value int, group *sync.WaitGroup) {
+	defer group.Done()
+
+	group.Add(1)
+	data.Store(value, value)
+}
+
+func TestMap(t *testing.T) {
+	data := &sync.Map{}
+	group := &sync.WaitGroup{}
+
+	for i := 0; i < 100; i++ {
+		go AddToMap(data, i, group)
+	}
+
+	group.Wait()
+
+	data.Range(func(key, value interface{}) bool {
+		fmt.Println(key, ":", value)
+		return true
+	})
+}
